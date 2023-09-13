11b56faeb068e6e638190ac8d0d734a28e7ba435
Ricardo Alves da Silva <ricardcpu@gmail.com>
2023-09-13 14:37:05 +0000

    fix: make request after cache.get

--- web/services.py
+++ web/services.py
@@ -22,11 +22,12 @@ class SnpData:
         self.snpid = snpid
 
         url = f"https://api.ncbi.nlm.nih.gov/variation/v0/refsnp/{snpid[2:]}"
-        response = requests.get(url)
 
         snp_data = cache.get(snpid, None)
 
         if snp_data == None:
+            response = requests.get(url)
+
             if response.status_code == 200:
                 snp_data = response.json()
                 cache.set(snpid, snp_data)
