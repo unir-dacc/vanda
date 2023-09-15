8f8802b93c9f1ae9912523457d2077764eeffc5a
Ricardo Alves da Silva <ricardcpu@gmail.com>
2023-09-15 15:56:31 -0400

    fix: update nix-shell

--- shell.nix
+++ shell.nix
@@ -2,18 +2,18 @@
 with pkgs;
 
 mkShell {
-	name = "dbNGEN";
+  name = "dbNGEN";
 
 # Add executable packages to the nix-shell environment.
-	packages = with pkgs; [
-		python311
-		pipenv
-		nodePackages.prettier
-	];
+  packages = with pkgs; [
+    python311
+    pipenv
+  ];
 
 # Bash statements that are executed by nix-shell.
-	shellHook = ''
-		pipenv shell
-		'';
-    DJANGO_SETTINGS_MODULE="dbNGEN.settings_dev";
+  shellHook = ''
+    pipenv shell
+    pre-commit install
+    '';
+  DJANGO_SETTINGS_MODULE="dbNGEN.settings_dev";
 }
