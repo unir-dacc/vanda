224a10f3f4007e1a2e7ee44b527fa1a06a23fd8e
Ricardo Alves da Silva <ricardcpu@gmail.com>
2023-09-16 13:20:50 -0400

    feature: update .nvim.lua

--- .nvim.lua
+++ .nvim.lua
@@ -1,1 +1,11 @@
 local keymap = vim.api.nvim_set_keymap
+
+local lspconfig = require("lspconfig")
+local null_ls = require("null-ls")
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+lspconfig.pyright.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
