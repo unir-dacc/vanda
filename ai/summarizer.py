4d32b94ae36f021f8cba5ed9191ed0d2f8dbc336
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-02-07 17:32:41 -0400

    feat: add accelerate python package

--- ai/summarizer.py
+++ ai/summarizer.py
@@ -3,7 +3,8 @@ from transformers import pipeline
 
 def summarizer(articles):
 
-    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    summarizer = pipeline(
+        "summarization", model="facebook/bart-large-cnn", device_map="auto")
 
     for article in articles:
         article["abstract"] = summarizer(article["abstract"], do_sample=False)
