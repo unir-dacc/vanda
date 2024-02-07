ec8db8d1ed90232cf667312e654d353d4f14017b
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-02-07 10:04:32 -0400

    feat: setup routes

--- ai/views.py
+++ ai/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import logging
+from ai.summarizer import summarizer
+from web import services
 
-# Create your views here.
+
+def gene_summary_abstracts(request, geneid=None):
+
+    abstracts = services.get_abstracts_by_gene(geneid)
+
+    summary_abstracts = summarizer(abstracts)
+
+    data = {'abstracts': summary_abstracts}
+
+    return JsonResponse(data)
+
+
+def snp_summary_abstracts(request, snpid=None):
+    abstracts = services.get_abstracts_by_snp(snpid)
+
+    summary_abstracts = summarizer(abstracts)
+
+    data = {'abstracts': summary_abstracts}
+
+    return JsonResponse(data)
