dbe16ddb07e7e434b3b20e6de43c52d6841af348
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-03-03 12:05:57 -0400

    feat: add biomedical-ner-all tokenizer

---
+++ ai/tokens.py
@@ -0,0 +1,44 @@
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+
+
+tokenizer = AutoTokenizer.from_pretrained("d4data/biomedical-ner-all")
+model = AutoModelForTokenClassification.from_pretrained(
+    "d4data/biomedical-ner-all")
+
+pipe = pipeline("ner", model=model, tokenizer=tokenizer,
+                aggregation_strategy="simple")
+
+characters = [',', '.', '!', '?', ';', '"', "'", " ", '(', ')']
+
+considerable_tokens = ["Disease_disorder"]
+
+context = 130
+
+
+def tokenize(text):
+    result = pipe(text['abstract'])
+
+    tokens = []
+    words = []
+
+    for item in result:
+        if item['entity_group'] in considerable_tokens:
+            token = {}
+            while text['abstract'][item['start']] not in characters:
+                item['start'] -= 1
+
+            while text['abstract'][item['end']] not in characters:
+                item['end'] += 1
+
+            word = text['abstract'][item['start'] + 1:item['end']]
+
+            if word not in words:
+                token['word'] = word
+                token['pmid'] = text['pmid']
+                token['title'] = text['title']
+                token['abstract'] = text['abstract']
+                tokens.append(token)
+                words.append(word)
+
+    return tokens
