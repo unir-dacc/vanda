5f52346cfefb8010aa097626666d30d1e1f144a3
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-03-14 14:58:22 -0400

    chore: add djhtml and autopep8 in flake.nix

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
       devShell.${system} = with pkgs;
 
         mkShell rec {
-          packages = [ python311 pipenv nodejs ];
+          packages = [ python311 pipenv nodejs python311Packages.autopep8 djhtml ];
           shellHook = ''
             pipenv shell --fancy
             pre-commit install
