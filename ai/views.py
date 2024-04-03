e0d363b28491d6f01a7d7c8221952998f3b3a84a
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-04-03 10:08:58 -0400

    fix: remove duplicate articles

--- ai/views.py
+++ ai/views.py
@@ -1,34 +1,41 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from ai.summarizer import summary
-from ai.tokens import tokenize
+from ai.tokens import tokenizer
 from web import services
 
 
-def render_topics(abstracts):
-    tokens = []
+def render_topics(articles):
 
-    for text in abstracts:
-        tokens += tokenize(text)
+    for item in articles:
+        item["tokens"] = tokenizer(item["abstract"])
 
     topics = {}
-    for item in tokens:
-        if topics.get(item['word'].lower()):
-            topics[item['word'].lower()].append(item)
-            item['abstract'] = summary(item['abstract'])
-        else:
-            topics[item['word'].lower()] = [item]
-            item['abstract'] = summary(item['abstract'])
+
+    for article in articles:
+        for item in article["tokens"]:
+            topics[item] = []
+
+    for key in topics:
+        for article in articles:
+            if key in article["tokens"]:
+                topics[key].append(
+                    {
+                        "pmid": article["pmid"],
+                        "title": article["title"],
+                        "abstract": article["abstract"]
+                    }
+                )
 
     return topics
 
 
 def snp_page(request, snpid):
-    abstracts = services.get_abstracts_by_snp(snpid)
+    articles = services.get_abstracts_by_snp(snpid)
 
-    topics = render_topics(abstracts)
+    topics = render_topics(articles)
 
     return render(request, 'web/snp.html', {"snp_name": "rs" + snpid, "data": topics})
+
 
 def gene_page(request, geneid):
     abstracts = services.get_abstracts_by_gene(geneid)
