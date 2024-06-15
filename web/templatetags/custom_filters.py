4a7336745d560d1b96d39012d0833b328e2a5970
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-06-15 17:29:13 -0400

    chore: add new django filter split

---
+++ web/templatetags/custom_filters.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def split(value, delimiter=':', index=0):
+    parts = value.split(delimiter)
+    try:
+        return parts[index]
+    except IndexError:
+        return ''
