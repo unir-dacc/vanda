f2801c18272d36ca05ed61245e93534a6acd99c3
Thauan Costa da Silva <thauansilva243@gmail.com>
2024-06-27 09:36:43 -0400

    feat: add collapsible button to HGVS on search page

--- web/static/web/script.js
+++ web/static/web/script.js
@@ -29,3 +29,18 @@ function setParam(name, value) {
   /* execute search */
   l.search = search;
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
