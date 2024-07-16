cca5103f31205924e50809113c09f5cd80b74726
Thauan <thauansilva243@gmail.com>
2024-07-16 16:07:27 -0400

    fix: collapsible button to search.html

--- web/static/web/script.js
+++ web/static/web/script.js
@@ -29,18 +29,3 @@ function setParam(name, value) {
   /* execute search */
   l.search = search;
 }
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
