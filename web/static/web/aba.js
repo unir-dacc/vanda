ff6715e43cc78f9b0095b5083d14244d71641dc8
Thauan <thauansilva243@gmail.com>
2024-08-18 13:49:54 -0400

    feat: button to switch between snp and keywords on gene page

---
+++ web/static/web/aba.js
@@ -0,0 +1,8 @@
+function showSNP(){
+    document.getElementById("snp-container").style.display = "block";
+    document.getElementById("keywords-container").style.display = "none";
+  }
+  function showKeywords(){
+    document.getElementById("snp-container").style.display = "none";
+    document.getElementById("keywords-container").style.display = "block";
+  }
