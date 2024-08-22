8364dd8c66e1646af6283a1ffd9f53facbdd568e
Thauan <thauansilva243@gmail.com>
2024-08-22 09:59:46 -0400

    refactor: javascript file to keywords/snp button

--- web/static/web/aba.js
+++
@@ -1,8 +0,0 @@
-function showSNP(){
-    document.getElementById("snp-container").style.display = "block";
-    document.getElementById("keywords-container").style.display = "none";
-  }
-  function showKeywords(){
-    document.getElementById("snp-container").style.display = "none";
-    document.getElementById("keywords-container").style.display = "block";
-  }
