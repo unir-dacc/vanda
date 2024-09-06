97502a8798bc4d9ff925fe04d9ec9351fc179ec8
Thauan <thauansilva243@gmail.com>
2024-09-05 23:28:46 -0400

    fix: filter checkbox functionality

--- web/static/web/script.js
+++ web/static/web/script.js
@@ -29,3 +29,59 @@ function setParam(name, value) {
 	/* execute search */
     l.search = search;
 }
+
+//filtro para os genes
+
+function verifyCheck(){
+    const singleGene = document.getElementById('singleGene');
+    const multipleGenes = document.getElementById('multipleGenes');
+
+    if(window.location.href.includes("single_gene=1")){
+        singleGene.checked = true;
+        multipleGenes.checked = false;
+    }
+    else if(window.location.href.includes("multiple_genes=1")){
+        singleGene.checked = false;
+        multipleGenes.checked = true;
+    }
+    else{
+        singleGene.checked = false;
+        multipleGenes.checked = false;
+    }
+    singleGene.addEventListener('change',function(){
+        if(singleGene.checked){
+            multipleGenes.checked = false;
+            let currentUrl = window.location.href;
+            if (!currentUrl.includes("single_gene=1") && !currentUrl.includes("multiple_genes=1")) {
+                let newUrl = currentUrl.includes("?") ? `${currentUrl}&single_gene=1` : `${currentUrl}?single_gene=1`;
+                window.location.href = newUrl;
+            }else if(!currentUrl.includes("single_gene=1") && currentUrl.includes("multiple_genes=1")) {
+                currentUrl = currentUrl.replace(/(\?|&)multiple_genes=1/, "");
+                newUrl = currentUrl.includes("?") ? `${currentUrl}&single_gene=1` : `${currentUrl}?single_gene=1`;
+                window.location.href = newUrl;
+            }
+        }else{
+            let newUrl = window.location.href.replace(/(\?|&)single_gene=1/, "");
+            window.location.href = newUrl;
+        }
+    });
+
+    multipleGenes.addEventListener('change',function(){
+        if(multipleGenes.checked){
+            singleGene.checked = false;
+            let currentUrl = window.location.href;
+            if (!currentUrl.includes("multiple_genes=1") && !currentUrl.includes("single_gene=1")) {
+                let newUrl = currentUrl.includes("?") ? `${currentUrl}&multiple_genes=1` : `${currentUrl}?multiple_genes=1`;
+                window.location.href = newUrl;
+            }else if(!currentUrl.includes("multiples_genes=1") && currentUrl.includes("single_gene=1")) {
+                currentUrl = currentUrl.replace(/(\?|&)single_gene=1/, "");
+                newUrl = currentUrl.includes("?") ? `${currentUrl}&multiple_genes=1` : `${currentUrl}?multiple_genes=1`;
+                window.location.href = newUrl;
+            }
+        }else{
+            let newUrl = window.location.href.replace(/(\?|&)multiple_genes=1/, "");
+            window.location.href = newUrl;
+        }
+    });
+}
+document.addEventListener('DOMContentLoaded', verifyCheck);
