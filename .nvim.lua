f1d66b5bed61678ada9d74beb3408558b86a6fa2
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-10-19 12:49:10 -0400

    chore: add ruff formatter

--- .nvim.lua
+++ .nvim.lua
@@ -5,12 +5,12 @@ local null_ls = require("null-ls")
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-lspconfig.pyright.setup({
+lspconfig.ruff.setup({
 	on_attach = on_attach,
 	capabilities = capabilities,
 })
 
-local null_ls = require("null-ls")
-local lspconfig = require("lspconfig")
-local opts = { noremap = true, silent = true }
-local keymap = vim.api.nvim_set_keymap
+lspconfig.pyright.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
