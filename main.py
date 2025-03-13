4dfe01ca6569735e2be040455a953164e3059628
Ricardo Alves da Silva <ricardcpu@gmail.com>
2025-03-13 14:53:47 -0400

    fix(api): Add * on CORS

--- main.py
+++ main.py
@@ -4,9 +4,20 @@ from app.routers import gene, search, snp
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 
+from fastapi.middleware.cors import CORSMiddleware
+
 ROOT_PATH = os.getenv("FASTAPI_ROOT_PATH", "/api")
 
 app = FastAPI(root_path=ROOT_PATH)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 add_pagination(app)
 
